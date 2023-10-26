2db7392c69fb1ffdb0d42bde45a2e8783d4b7a4c
Lady Robalino <lyrobalino1@utpl.edu.ec>
2023-10-26 18:02:39 -0500

    Listooo!

---
+++ Ejercicio04/src/ejercicio04/Ejercicio04.java
@@ -0,0 +1,23 @@
+package ejercicio04;
+public class Ejercicio04 {
+    public static void main(String[] args) {
+    //Resolucion del problema
+            int var=20;
+            int var1=1;
+            int var2=3;
+            int var3=2;
+           boolean Respuesta;
+                   Respuesta = (var/(var1+var2))+var3>=var2 ;
+           System.out.println("Respuesta en boolean: "+ Respuesta);
+           int RespuestaNum ;
+               RespuestaNum = (var/(var1+var2))+var3 ;
+           System.out.println("Respuesta correcta es: "+ RespuestaNum);
+    }
+    }
+            
+            
+    
+     
+    
+    
+}
