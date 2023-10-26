9b25bcb1452f03ab714ff03a36b7d97862c5b1f2
Lady Robalino <lyrobalino1@utpl.edu.ec>
2023-10-26 17:21:50 -0500

    respuestaEjercicio02

--- Ejercicio02/src/ejercicio02/Ejercicio02.java
+++ Ejercicio02/src/ejercicio02/Ejercicio02.java
@@ -1,28 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio02;
-
-/**
- *
- * @author reroes
- */
 public class Ejercicio02 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // Resolución del problema
         int var1 = 10;
-        int var2 = 2;
+        int var2 = 10;
+        double respuesta;
+        respuesta = Math.pow(var1, 2) + var2;
+        System.out.println("La respuesta es:" + respuesta);
+        //Autor LadyR
         
-                
-    }
-    
-    
-    
+        
+        }
     
 }
